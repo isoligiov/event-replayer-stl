9baa05e68b4ec9f4e7d70beaf4fb1ff8c6a4e6e8
mac <mac@macs-MacBook-Pro-3.local>
2024-12-18 09:34:26 -0500

    install script update

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
+#!/bin/zsh
+
 cd "$(dirname "$0")";
 
 python3 -m venv env
 source env/bin/activate
 pip3 install -r requirements.txt
+
+read "channelId?CHANNEL_ID: "
+outputFile=".env"
+(
+  echo "CHANNEL_ID=$channelId"
+) > $outputFile
+
+echo ".env initialized"
